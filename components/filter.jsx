52ef4fac1b2ba963d3e80f252fee8adee7c6748e
Robert O'Connor <robertgoconnor2@gmail.com>
2018-08-14 15:57:20 -0400

    Create filter component skeleton

---
+++ components/filter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Filter extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Filter;
